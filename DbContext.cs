c48064dd0a764eca10641da24eb88c6cd957464b
acer <acer@LAPTOP-U520QGBT>
2024-03-12 01:05:34 +0200

    Add project files.

---
+++ DbContext.cs
@@ -0,0 +1,18 @@
+﻿// Data/AppDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleAPI.Data;
+using SimpleAPI.Models;
+
+namespace SimpleAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
+
---
+++ Item.cs
@@ -0,0 +1,16 @@
+﻿// Models-Item.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
